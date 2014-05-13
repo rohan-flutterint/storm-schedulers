83680ae645961b18d6fd22c0bc637333a10574d8
Lasantha Fernando <lasantha.fdo@gmail.com>
2014-05-13 15:07:52 +0530

    Fixed duplicate supervisor issue

--- src/main/java/org/wso2/siddhi/storm/scheduler/RoundRobinStormScheduler.java
+++ src/main/java/org/wso2/siddhi/storm/scheduler/RoundRobinStormScheduler.java
@@ -13,7 +13,7 @@ public class RoundRobinStormScheduler implements IScheduler {
     public static final String SIDDHI_TOPOLOGY_NAME = "LatencyMeasureTopology";
     private static Log log = LogFactory.getLog(RoundRobinStormScheduler.class);
     Map conf;
-    Map<String, List<SupervisorDetails>> componentToSupervisor = new HashMap<String, List<SupervisorDetails>>();
+    Map<String, Map<String, SupervisorDetails>> componentToSupervisor = new HashMap<String, Map<String, SupervisorDetails>>();
 
     public void prepare(Map conf) {
         this.conf = conf;
@@ -25,19 +25,13 @@ public class RoundRobinStormScheduler implements IScheduler {
             String componentId = (String) meta.get("dedicated.to.component");
 
             if (componentId != null && !componentId.isEmpty()) {
-                List<SupervisorDetails> supervisorList = componentToSupervisor.get(componentId);
-                if (supervisorList == null) {
-                    supervisorList = new ArrayList<SupervisorDetails>();
-                    componentToSupervisor.put(componentId, supervisorList);
+                Map<String, SupervisorDetails> supervisorSet = componentToSupervisor.get(componentId);
+                if (supervisorSet == null) {
+                    supervisorSet = new HashMap<String, SupervisorDetails>();
+                    componentToSupervisor.put(componentId, supervisorSet);
                 }
-                supervisorList.add(supervisor);
+                supervisorSet.put(supervisor.getId(), supervisor);
             }
-        }
-    }
-
-    private void freeAllSlots(SupervisorDetails supervisor, Cluster cluster) {
-        for (Integer port : cluster.getUsedPorts(supervisor)) {
-            cluster.freeSlot(new WorkerSlot(supervisor.getId(), port));
         }
     }
 
@@ -60,67 +54,70 @@ public class RoundRobinStormScheduler implements IScheduler {
                 for (Map.Entry<String, List<ExecutorDetails>> entry : componentToExecutors.entrySet()) {
                     String componentName = entry.getKey();
                     List<ExecutorDetails> executors = entry.getValue();
-                    List<SupervisorDetails> supervisorList = componentToSupervisor.get(componentName);
-                    if (supervisorList != null && !supervisorList.isEmpty()) {
-                        List<WorkerSlot> availableSlots = new ArrayList<WorkerSlot>();
-                        List<Queue<WorkerSlot>> supervisorAvailableSlots = new ArrayList<Queue<WorkerSlot>>();
-                        List<String> supervisorHostList = new ArrayList<String>();
-                        for (SupervisorDetails supervisor : supervisorList) {
-                            Queue<WorkerSlot> slotQ = new LinkedBlockingQueue<WorkerSlot>();
-                            slotQ.addAll(cluster.getAvailableSlots(supervisor));
-                            supervisorAvailableSlots.add(slotQ);
-                            supervisorHostList.add(supervisor.getHost());
-                        }
-
-                        int supervisorIndex = -1;
-                        int supervisorCount = supervisorList.size();
-                        while (!supervisorAvailableSlots.isEmpty()) {
-                            supervisorIndex = (supervisorIndex + 1) % supervisorCount;
-                            Queue slotQ = supervisorAvailableSlots.get(supervisorIndex);
-                            if (slotQ.isEmpty()) {
-                                supervisorAvailableSlots.remove(supervisorIndex);
-                                supervisorCount--;
-                                continue;
+                    if (componentToSupervisor.get(componentName) != null) {
+                        Collection<SupervisorDetails> dedicatedSupervisorSet = componentToSupervisor.get(componentName).values();
+                        if (!dedicatedSupervisorSet.isEmpty()) {
+                            List<WorkerSlot> availableSlots = new ArrayList<WorkerSlot>();
+                            List<Queue<WorkerSlot>> supervisorAvailableSlots = new ArrayList<Queue<WorkerSlot>>();
+                            List<String> supervisorHostList = new ArrayList<String>();
+                            for (SupervisorDetails supervisor : dedicatedSupervisorSet) {
+                                Queue<WorkerSlot> slotQ = new LinkedBlockingQueue<WorkerSlot>();
+                                slotQ.addAll(cluster.getAvailableSlots(supervisor));
+                                supervisorAvailableSlots.add(slotQ);
+                                supervisorHostList.add(supervisor.getHost());
                             }
-                            availableSlots.add((WorkerSlot) slotQ.poll());
-                        }
 
-                        if (log.isDebugEnabled()) {
-                            log.debug("Scheduling " + componentName + " to supervisors " + Arrays.toString(supervisorHostList.toArray(new String[supervisorHostList.size()])));
-                            log.debug("Available slots: ");
-                            for (WorkerSlot availableSlot : availableSlots) {
-                                log.debug("Node ID: " + availableSlot.getNodeId());
+                            int supervisorIndex = -1;
+                            int supervisorCount = dedicatedSupervisorSet.size();
+                            while (!supervisorAvailableSlots.isEmpty()) {
+                                supervisorIndex = (supervisorIndex + 1) % supervisorCount;
+                                Queue slotQ = supervisorAvailableSlots.get(supervisorIndex);
+                                if (slotQ.isEmpty()) {
+                                    supervisorAvailableSlots.remove(supervisorIndex);
+                                    supervisorCount--;
+                                    continue;
+                                }
+                                availableSlots.add((WorkerSlot) slotQ.poll());
                             }
-                        }
 
-
-                        if (availableSlots.isEmpty()) {
-                            throw new RuntimeException("No free slots available for scheduling in dedicated supervisors @ " + Arrays.toString(supervisorHostList.toArray(new String[supervisorHostList.size()])));
-                        }
-
-                        int availableSlotCount = availableSlots.size();
-                        int executorCount = executors.size();
-
-                        List<List<ExecutorDetails>> executorsForSlots = new ArrayList<List<ExecutorDetails>>();
-                        for (int i = 0; i < availableSlotCount; i++) {
-                            executorsForSlots.add(new ArrayList<ExecutorDetails>());
-                        }
-
-                        for (int i = 0; i < executorCount; i++) {
-                            int slotToAllocate = i % availableSlotCount;
-                            ExecutorDetails executor = executors.get(i);
-                            executorsForSlots.get(slotToAllocate).add(executor);
-                        }
-
-                        for (int i = 0; i < availableSlotCount; i++) {
-                            if (!executorsForSlots.get(i).isEmpty()) {
-                                cluster.assign(availableSlots.get(i), siddhiTopology.getId(), executorsForSlots.get(i));
+                            if (log.isDebugEnabled()) {
+                                log.debug("Scheduling " + componentName + " to supervisors " + Arrays.toString(supervisorHostList.toArray(new String[supervisorHostList.size()])));
+                                log.debug("Available slots: ");
+                                for (WorkerSlot availableSlot : availableSlots) {
+                                    log.debug("Node ID: " + availableSlot.getNodeId());
+                                }
                             }
+
+
+                            if (availableSlots.isEmpty()) {
+                                throw new RuntimeException("No free slots available for scheduling in dedicated supervisors @ " + Arrays.toString(supervisorHostList.toArray(new String[supervisorHostList.size()])));
+                            }
+
+                            int availableSlotCount = availableSlots.size();
+                            int executorCount = executors.size();
+
+                            List<List<ExecutorDetails>> executorsForSlots = new ArrayList<List<ExecutorDetails>>();
+                            for (int i = 0; i < availableSlotCount; i++) {
+                                executorsForSlots.add(new ArrayList<ExecutorDetails>());
+                            }
+
+                            for (int i = 0; i < executorCount; i++) {
+                                int slotToAllocate = i % availableSlotCount;
+                                ExecutorDetails executor = executors.get(i);
+                                executorsForSlots.get(slotToAllocate).add(executor);
+                            }
+
+                            for (int i = 0; i < availableSlotCount; i++) {
+                                if (!executorsForSlots.get(i).isEmpty()) {
+                                    cluster.assign(availableSlots.get(i), siddhiTopology.getId(), executorsForSlots.get(i));
+                                }
+                            }
+                        } else {
+                            log.info("No dedicated supervisor for " + componentName);
                         }
-                    } else {
-                        log.info("No dedicated supervisor for " + componentName);
                     }
                 }
+
             }
         }
         new EvenScheduler().schedule(topologies, cluster);
